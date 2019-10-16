213bb792b79c7f192ce6e34faed5e2c269814e6e
Pearly-choong <choongpl@gmail.com>
2019-10-16 15:32:43 +1300

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+console.log('forms and js');
+
+//reading form date
+  document.getElementById('submit').addEventListener('click', function(){
+  var name = document.getElementById('name').value;
+  console.log(name);
+
+  var address = document.getElementById('address').value;
+  console.log(address);
+
+  document.getElementById('formData').innerHTML = '</br>' + name + '</br>' + address;
+  // target based on tag Name, targeted first div, the index starts a 0
+  //document.getElementsByTagName('div')[0].innerHTML = '</br>' + name + '</br>' + address;
+});
+
+
+var names=[];
+var projectA = [];
+var projectB = [];
+var projectC = [];
+var averagePerformance = [];
+
+var n = parseInt(prompt('Enter the number of employees'));
+
+document.getElementById('saveData').addEventListener('click', function(){
+  var ename = document.getElementById('ename').value;
+  console.log(ename);
+
+  var projA = document.getElementById('projectA').value;
+  console.log(projA);
+
+  var projB = document.getElementById('projectB').value;
+  console.log(projB);
+
+  var projC = document.getElementById('projectC').value;
+  console.log(projC);
+
+  var average = (projA + projB + projC)/3;
+  console.log(average);
+
+  document.getElementsByTagName('div')[0].innerHTML
+  += '</br>' + ename + '</br>' + projA + '</br>' + projB + '</br>' + projC;
+
+  names.push(ename);
+  projectA.push(projA);
+  projectB.push(projB);
+  projectC.push(projC);
+  averagePerformance.push(average);
+  console.log(names, projectA, projectB, projectC, averagePerformance);
+
+
+});
